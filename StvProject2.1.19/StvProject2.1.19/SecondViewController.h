b958e9016d50e13f083eff094f3517ce221b80e1
竹本大地 <doughnutriders@gmail.com>
2018-05-24 10:09:56 +0900

    [Fixed]#2.1.19 First commit.

---
+++ StvProject2.1.19/StvProject2.1.19/SecondViewController.h
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.h
+//  StvProject2.1.19
+//
+//  Created by 竹本大地 on 2018/05/21.
+//  Copyright © 2018年 Daichi Take. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+// 受け取り用ラベルのプロパティを定義
+@property (strong, nonatomic) NSString *receiveString;
+
+@end
