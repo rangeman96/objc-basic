13adf695df77e93e66abbaf54e12fb7e0424553f
竹本大地 <doughnutriders@gmail.com>
2018-05-29 10:34:29 +0900

    [Update] # 2.1.16 課題を修正しました。

---
+++ StvProject2.1.16-2/StvProject2.1.16-2/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  StvProject2.1.16-2
+//
+//  Created by 竹本大地 on 2018/05/28.
+//  Copyright © 2018年 Daichi.T96. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ StvProject2.1.16-2/StvProject2.1.16-2/SecondViewController.m
@@ -0,0 +1,37 @@
+//
+//  SecondViewController.m
+//  StvProject2.1.16-2
+//
+//  Created by 竹本大地 on 2018/05/28.
+//  Copyright © 2018年 Daichi.T96. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
