da12852cf089a0a480e55c30237a7aa5596a860f
竹本大地 <doughnutriders@gmail.com>
2018-05-29 20:58:13 +0900

    [Fixed]#2.4.6 First commit.

---
+++ StvProject2.4.6/Pods/Target Support Files/Pods-StvProject2.4.6/Pods-StvProject2.4.6-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StvProject2_4_6 : NSObject
+@end
+@implementation PodsDummy_Pods_StvProject2_4_6
+@end
---
+++ StvProject2.4.6/StvProject2.4.6/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  StvProject2.4.6
+//
+//  Created by 竹本大地 on 2018/05/29.
+//  Copyright © 2018年 Daichi.T96. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
+
