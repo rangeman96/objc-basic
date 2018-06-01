e4d98a06d7f063b9b171391db26f6e77dfff5cbb
竹本大地 <doughnutriders@gmail.com>
2018-06-01 16:29:20 +0900

    [Fixed]#2.6.1 First commit.

---
+++ StvProject2.6.1/StvProject2.6.1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  StvProject2.6.1
+//
+//  Created by 竹本大地 on 2018/06/01.
+//  Copyright © 2018年 Daichi.T96. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
